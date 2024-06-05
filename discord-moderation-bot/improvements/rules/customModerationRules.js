88329e82d8b194192a4e4831004af8da7eb53ef6
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-06-05 16:37:46 +0530

    Added: `discord-moderation-bot/improvements/rules/customModerationRules.js`

---
+++ discord-moderation-bot/improvements/rules/customModerationRules.js
@@ -0,0 +1,22 @@
+const customModerationRules = {
+  automatedKickAndBan: true,
+  customizableWarningSystem: true,
+  profanityFilter: true,
+  messageLogging: true,
+  roleManagement: true,
+  scheduledMessages: true,
+  userEngagementTools: {
+    polls: true,
+    giveaways: true,
+  },
+  customCommands: true,
+  thirdPartyIntegration: true,
+  userFriendlyDashboard: true,
+  reportingSystem: true,
+  aiPoweredModeration: true,
+  userVerificationSystem: true,
+  moderationQueue: true,
+  userReputationSystem: true,
+};
+
+module.exports = customModerationRules;
