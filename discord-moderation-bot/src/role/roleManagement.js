566177130fcc32c73ef2974f005a32d3beea57bc
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-06-05 16:37:02 +0530

    Added: `discord-moderation-bot/src/role/roleManagement.js`

---
+++ discord-moderation-bot/src/role/roleManagement.js
@@ -0,0 +1,16 @@
+const roleManagement = {
+  assignRole: (user, role) => {
+    // Logic to assign a role to a user
+  },
+  revokeRole: (user, role) => {
+    // Logic to revoke a role from a user
+  },
+  getRoles: (user) => {
+    // Logic to get all roles of a user
+  },
+  getMembersWithRole: (role) => {
+    // Logic to get all members with a specific role
+  },
+};
+
+module.exports = roleManagement;
